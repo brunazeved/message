ee01fb88f396618f33bf1c16f32d80945a908360
Bruna Azevedo <102159080+brunazeved@users.noreply.github.com>
2022-04-18 21:12:42 -0300

    :art: Add Auth Token
    Add Auth Token, SID n import Client

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import pandas as pd
+from twilio.rest import Client
+# Minha conta SID
+account_sid = "AC1fc31f00b38482595ff700e8f93944f5"
+# Meu Auth Token
+auth_token  = "d53c7c1f602f848f326edecd87200596"
+client = Client(account_sid, auth_token)
 
 lmeses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
