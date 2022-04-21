9198cded4ba087d56c4699aa881eee7c8b090c60
Bruna Azevedo <102159080+brunazeved@users.noreply.github.com>
2022-04-20 23:24:04 -0300

    :art: Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ for mes in lmeses:
         vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
         vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
         print(f'No mês {mes} alguém bateu a meta. Vendedor:{vendedor} - Vendas:{vendas}')
+        message = client.messages.create(
+                    to="+5551984886296",
+                    from_="+17406854437",
+                    body=f"No mês {mes} alguém bateu a meta. Vendedor:{vendedor} - Vendas:{vendas}")
+        print(message.sid)
